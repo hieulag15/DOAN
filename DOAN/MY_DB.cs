c0ec5e9a8d61a4c4a16b85d38d83c1d78e8e0709
hieulag15 <hieuthanhtran12@gmail.com>
2023-10-18 12:02:52 +0700

    Add project files.

---
+++ DOAN/MY_DB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOAN
+{
+    internal class MY_DB
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=HIEULAG\THANHHIEU;Initial Catalog=CUAHANG;Integrated Security=True");
+
+        public SqlConnection getConnection
+        {
+            get
+            {
+                return conn;
+            }
+        }
+
+
+        //open the connection
+        public void openConnection()
+        {
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+        }
+
+        //close the connection
+        public void closeConnection()
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+    }
+}
